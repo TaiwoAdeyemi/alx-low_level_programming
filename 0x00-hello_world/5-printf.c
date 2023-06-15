e7e5b2935684765c72305c64d5eacf4602d66a90
Taiwo Adeyemi <tadeyemi004@outlook.com>
2023-06-15 16:56:16 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ * print meassage
+ * Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
