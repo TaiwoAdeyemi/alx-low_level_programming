4c0d767c845672e4d239a783079213d9683d8eb4
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-19 16:44:05 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print from 0 to 9.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
