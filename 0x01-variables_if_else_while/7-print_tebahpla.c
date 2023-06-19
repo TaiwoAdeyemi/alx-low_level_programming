0fbaadaac7dcc071753111810db4e64d734708d0
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-19 16:51:20 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print abc reverse.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
