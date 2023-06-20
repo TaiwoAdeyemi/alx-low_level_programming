2ce0200eeb21ca3882bbbb87acd7261e5d629514
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-19 17:06:59 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints out all possible combos of 3 digits
+ * Return: 0
  */
 int main(void)
 {
-	int i, j;
 
-	for (i = 0; i < 99; i++)
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
 	{
-		for (j = i + 1; j < 100; j++)
+		for (j = i; j < 58; j++)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
-			putchar(32);
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-			if (i != 98 || j != 99)
+			for (k = j; k < 58; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				if (i == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
 			}
 		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
+
