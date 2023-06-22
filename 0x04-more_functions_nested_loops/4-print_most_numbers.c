8a86e36dec8a10715318b92e366747d0803cf6f5
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-22 16:32:14 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' || i != '4')
+			;
+		else
+			_putchar('0" + i);
+	}
+	_putchar('\n');
+}
