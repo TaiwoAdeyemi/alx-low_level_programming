1a40f4a1531b8d99c93edd3ba4e83134a1e05ba6
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-22 16:08:50 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _-isdigit - input
+ *
+ * Description: check for a number
+ * @c: charater
+ * Return: 1 yes 0 no
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0):
+}
