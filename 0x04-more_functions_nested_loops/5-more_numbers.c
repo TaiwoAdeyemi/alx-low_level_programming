f7c25bf8d15f686e4590a7c8b1518891dd3aed4c
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-22 16:56:17 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,17 @@
 
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for (j = 0; j < 10; j++)
-    {
-        for (i = 0; i <= 14; i++)
-        {
-            if (i > 9)
-		    _putchar(i / 10 + '0');
-	    
-	    _putchar(i % 10 + '0');
-        }
-        _putchar('\n');
-    }
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
