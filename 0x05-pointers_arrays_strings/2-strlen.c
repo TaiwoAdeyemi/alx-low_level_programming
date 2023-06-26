34a0c39db64cdc9bdf0f23f941efad7f32c1989b
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-26 14:24:12 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,18 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 /**
- *_strlen - the length of a string.
- *@s: char pointer.
- *Return: length of a string.
- */
-
+  * _strlen - Returns the length of a string.
+  * @s: char pointer.
+  * Return: length of a string.
+  */
 int _strlen(char *s)
 {
 	int length = 0;
 
+	
 	while (*s != 0)
 	{
 		length++;
 		s++;
 	}
-	
 	return (length);
 }
