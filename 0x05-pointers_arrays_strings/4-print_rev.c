e86095d2778d1f6a4fa9904297373d7a8365228e
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-26 15:12:51 -0700

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
-  * print_rev - Returns the length of a string.
-  * @s: char pointer.
-  * Return: length of a string.
-  */
+ * print_rev - prints a string in reverse order
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
 void print_rev(char *s)
 {
-	int count;
+int i;
 
-	while (*s != 0)
-	{
-		count++;
-		s++;
-	}
-	s--;
-	while (count - 1 >= 0)
-	{
-		_putchar(*s);
-		count--;
-		s--;
-	}
-	_putchar(10);
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (i = i - 1 ; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+
+_putchar ('\n');
 }
