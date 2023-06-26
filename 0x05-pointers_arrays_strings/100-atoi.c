59fd4c63fa0e57b7550f3476b52a64f187fed237
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-06-26 15:05:42 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,47 @@
 #include "main.h"
+
 /**
- * _atoi - convert a string to an integer.
- * @s: string to be converted.
- * Return: corverted number.
+ * is_numerical - check if it is a digit
+ * @n: Number
+ * Return: If is a number, return 1 else return 0
+ */
+int is_numerical(unsigned int n)
+{
+return (n >= '0' &&  n <= '9');
+}
+
+/**
+ * _atoi - convert a string to an integer
+ *@s: String
+ * Return: Return the num
  */
 int _atoi(char *s)
 {
-	int sign, i;
-	unsigned int num;
+unsigned int number, i;
+int sign;
 
-	num = 0, sign = 1, i = 0;
-	while (s[i] && (s[i] < 48 || s[i] > 57))
-	{
-		if (s[i] == 45)
-			sign *= -1;
-		i++;
+sign = 1;
+number = 0;
 
+
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (is_numerical(s[i]))
+{
+number = (s[i] - 48) + number * 10;
+
+if (s[i + 1] == ' ')
+break;
+}
+else if (s[i] == '-')
+{
+sign *= -1;
+}
+
+}
+
+return (number *sign);
+
+
+}
