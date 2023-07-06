28274779421fec63ace54276185daea8a2242728
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-07-06 03:41:38 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,14 @@
  *Return: void
  */
 
-void _puts_recursion(char *s)
-
+void _puts_recursion(char *s) 
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-
-_putchar (*s);
-_puts_recursion(s + 1);
-
+    if (*s == '\0') 
+    {
+        printf("\n");
+        return;
+    }
+    
+    putchar(*s);
+    _puts_recursion(s + 1);
 }
