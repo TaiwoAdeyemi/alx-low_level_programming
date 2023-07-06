b2194bd310aa4d4d4bd419b8a645aa6b74f82938
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-07-06 03:30:50 -0700

    recurson

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,12 @@
   * Return: Print string.
   */
 
-void _puts_recursion(char *s)
-{
-	if (*s != 0)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-		return;
-	}
-} 	
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+        printf("\n");
+        return;
+    }
+
+    putchar(*s);
+    _puts_recursion(s + 1);
+}
