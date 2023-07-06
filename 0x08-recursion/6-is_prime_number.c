9cfe75cde73c0c523ea69684cbdccd2a517e4d42
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-07-06 04:02:21 -0700

    prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  * is_prime_number - integer is a prime number.
+  * @n: number of function.
+  * 
+  * Return: number prime.
+  */
+int prime_number(int n, int n_module)
+{
+	if (n % n_module != 0 && n > 2)
+	{
+		if (n_module > n / 2)
+			return (1);
+	}
+	else
+		return (0);
+	return (prime_number(n, n_module + 1));
+}
+/**
+  * is_prime_number - find prime number.
+  * @n: number.
+  *
+  * Return: number prime.
+  */
+int is_prime_number(int n)
+{
+	return (prime_number(n, 2));
+}
