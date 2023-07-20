1004774f776dfe9494f9d26d3815fa73f53c844e
TaiwoAdeyemi <Tadeyemi004@outlook.com>
2023-07-20 13:17:21 -0700

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,70 +1,88 @@
 #include "dog.h"
-/**
- * _strlen - return the len of string *s
- * @s: a string.
- * Return: len of string.
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int len = 0;
 
-	while (*(s + len))
-		len++;
-	return (len);
-}
+#include <stdlib.h>
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 /**
- * _strcpy - copy string
- * @s: string to copy
- * Return: copy string.
- */
-char *_strcpy(char *s)
-{
-	char *new_s;
-	unsigned int i, len;
-
-	len = _strlen(s);
-	new_s = malloc(sizeof(char) * (len + 1));
-	/* allocation fails */
-	if (!new_s)
-		return (NULL);
-	/* String copy */
-	for (i = 0; *(s + i); i++)
-		*(new_s + i) = *(s + i);
-	/* add character null */
-	*(new_s + i) = *(s + i);
-	/* return result */
-	return (new_s);
-}
-/**
-  * new_dog - creates a new dog.
-  * @name: name of dog.
-  * @age: age of dog.
-  * @owner: owner of dog.
-  * Return: NULL if fail otherwise a dog.
-  */
+* new_dog - function
+* @name: name of dog
+* @age: age of new doggo
+* @owner: owner of new doggo
+*
+* Description: function to create a new dog. Store copy of name & owner
+* Return: ptr, null if fail
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *cpy_name, *cpy_owner;
+	char *nename, *neowner;
+	int lename, leowner;
 
-	d = malloc(sizeof(dog_t));
-	if (!d)
+	dog_t *doggo = malloc(sizeof(dog_t));
+
+	if (doggo == NULL)
 		return (NULL);
-	/* Copy strings */
-	cpy_name = _strcpy(name);
-	cpy_owner = _strcpy(owner);
-	/* If fail someone free and return NULL */
-	if (!cpy_name || !cpy_owner)
-	{	free(d);
-		if (cpy_name)
-			free(cpy_name);
-		if (cpy_owner)
-			free(cpy_owner);
+	_strlen(name);
+	lename = _strlen(name);
+	nename = malloc((lename + 1) * sizeof(char));
+	if (nename == NULL)
+	{
+		free(doggo);
 		return (NULL);
 	}
-	/* Otherwise assing name and owner */
-	d->name = cpy_name;
-	d->age = age;
-	d->owner = cpy_owner;
-	return (d);
+	_strcpy(nename, name);
+
+	_strlen(owner);
+	leowner = _strlen(owner);
+	neowner = malloc((leowner + 1) * sizeof(char));
+	if (neowner == NULL)
+	{
+		free(nename);
+		free(doggo);
+		return (NULL);
+	}
+	_strcpy(neowner, owner);
+
+	doggo->name = nename;
+	doggo->age = age;
+	doggo->owner = neowner;
+
+	return (doggo);
+}
+
+/**
+* _strlen - function
+* @s: first operand & pointer
+*
+* Description: function that returns the length of a string
+* Return: Always 0
+*/
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (*s != '\0')
+	{
+		index++;
+		s++;
+	}
+	return (index);
+}
+/**
+* _strcpy - function
+* @src: copy from
+* @dest: copy to
+*
+* Description: copies strng pnted by src to dest with null
+* Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
